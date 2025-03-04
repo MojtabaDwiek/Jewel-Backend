7c39215122907df0c4d9622d167a9040472028d2
MojtabaDwiek <jakodwk717@gmail.com>
2025-03-04 21:11:48 +0000

    new

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,12 +38,15 @@ return [
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
-            'throw' => false,
-        ],
+        'driver' => 's3',
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION'),
+        'bucket' => env('AWS_BUCKET'),
+        'endpoint' => env('AWS_ENDPOINT'),
+        'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', true), // Required for R2
+        'visibility' => 'public', // Set visibility to public
+    ],
 
         's3' => [
             'driver' => 's3',
